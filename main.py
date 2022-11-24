81d56b13828f2f743e4726597a3017d60feb252e
Mame Khalifa <diopmame_khalifa7@yahoo.com>
2022-11-24 22:40:10 +0700

    boucle while

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 
-nom=input("quel est votre nom ")
-age=input("quel est votre age ")
+nom=input("quel est votre nom ? ")
+
 #CONVERSION D'UNE CHAINE EN ENTIER
 
 #GESTION DES ERREURS
-try:
-    age_prochain= int(age)+1
-except:
-    print("ERREUR: vous devez entrer un nombre")
+age_prochain=0
+while age_prochain == 0:
+    age = input("quel est votre age ? ")
+    try:
+    #en convertissant aussi on peut mettre
+    #age=int(input("quel est votre age  f"))
+        age_prochain= int(age)+1
+    except ValueError:
+        print("ERREUR: vous devez entrer un nombre")
 else:
     print(f"vous vous appelez {nom} et vous avez {age} ans")
     print("l'annee prochaine vous aurez "+str(age_prochain)+" ans")
