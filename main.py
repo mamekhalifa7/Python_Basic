391e5dcb053645d6ecfd9634b8569b4864f160aa
Mame Khalifa <diopmame_khalifa7@yahoo.com>
2022-11-24 21:32:32 +0700

    conversion d'une chaine en entier

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello word")
+
+nom=input("quel est votre nom ")
+age=input("quel est votre age ")
+#CONVERSION D'UNE CHAINE EN ENTIER
+age_prochain= int(age)+1
+print(f"vous vous appelez {nom} et vous avez {age} ans")
+print("l'annee prochaine vous aurez "+str(age_prochain)+" ans")
+print("MERCI")
