da29e44ccc531727811046b4a3fe7c1e026ef6c3
Mame Khalifa <diopmame_khalifa7@yahoo.com>
2022-11-24 21:42:47 +0700

    gestion des erreurs avec try except

--- main.py
+++ main.py
@@ -2,7 +2,13 @@
 nom=input("quel est votre nom ")
 age=input("quel est votre age ")
 #CONVERSION D'UNE CHAINE EN ENTIER
-age_prochain= int(age)+1
-print(f"vous vous appelez {nom} et vous avez {age} ans")
-print("l'annee prochaine vous aurez "+str(age_prochain)+" ans")
-print("MERCI")
+
+#GESTION DES ERREURS
+try:
+    age_prochain= int(age)+1
+except:
+    print("ERREUR: vous devez entrer un nombre")
+else:
+    print(f"vous vous appelez {nom} et vous avez {age} ans")
+    print("l'annee prochaine vous aurez "+str(age_prochain)+" ans")
+    print("MERCI")
