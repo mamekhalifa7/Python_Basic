c9c565dc171e3e5cd910d6b5ea45cb96d929d310
Mame_Khalifa <diopmame_khalifa7@yahoo.com>
2023-01-09 20:36:33 +0100

    avec une fonction

--- tortueGame.py
+++ tortueGame.py
@@ -1,12 +1,24 @@
 import turtle
 
 t = turtle.Turtle()
+"""
 for i in range (1,5):
     t.forward(30)
     t.left(90)
     t.forward(30)
     t.right(90)
 t.forward(30)
+"""
+# avec une fonction
+def escalier (taille,nb):
+    for i in range(0, nb):
+        t.forward(taille)
+        t.left(90)
+        t.forward(taille)
+        t.right(90)
+    t.forward(taille)
+
+escalier(30,5)
 
 turtle.done()
 
