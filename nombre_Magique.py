912434706ecae090e6502476c69fbcdddf0e25bd
Mame_Khalifa <diopmame_khalifa7@yahoo.com>
2023-01-09 22:03:13 +0100

    les conditions

---
+++ nombre_Magique.py
@@ -0,0 +1,16 @@
+def nombre_magique(min,max):
+    nbre=input(f"entrer le nombre magique entre {min} et {max} ")
+    nbre_int=int(nbre)
+    return nbre_int
+
+
+nmin=1
+nmax=10
+nmag=5
+nombre=nombre_magique(nmin,nmax)
+if nombre>nmag:
+    print("le nombre magique est plus petit")
+elif nombre<nmag:
+    print("le nombre magique est plus grand")
+else:
+     print("Bravo ! vous avez gagné")
