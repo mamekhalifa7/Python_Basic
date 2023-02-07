325368c931685411830880f2633d60fb36a86de1
Mame_Khalifa <diopmame_khalifa7@yahoo.com>
2023-02-07 05:44:41 +0100

    conversion avec les fonctions

--- conversion_unite_de_mesures.py
+++ conversion_unite_de_mesures.py
@@ -1,4 +1,6 @@
 
+# Raisonnement simple sans fonction
+"""
 print(" Ce programme vous permet d'effectuer des conversions d'unité")
 print("1 - pouces vers cm")
 print("2 - cm vers pouces")
@@ -8,3 +10,20 @@ if choice=="1":
     valeur_float = float(valeur_str)
     valeur_convertie = round(valeur_float * 2.54, 2)
 print(f"Resultat de conversion : {valeur_float} pouces = {valeur_convertie} cm")
+"""
+def conversion(unit1:str , unit2:str , facteur:float):
+    valeur_str= input(f"conversion {unit1} vers {unit2}. Donner la valeur en {unit1} ")
+    valeur_float= float(valeur_str)
+    valeur_convertie= round(valeur_float * facteur,2)
+    print()
+    print(f"Resultat de conversion : {valeur_float} {unit1} = {valeur_convertie} {unit2}")
+
+print(" Ce programme vous permet d'effectuer des conversions d'unité")
+print("1 - pouces vers cm")
+print("2 - cm vers pouces")
+choice = input("votre choix (1 ou 2): ")
+if choice=="1":
+    print()
+    conversion("pouce","cm",2.54)
+else:
+    conversion("cm","pouce",0.394)
