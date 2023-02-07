d861c2d4caf883157ec108b832bcd806a1bcad23
Mame_Khalifa <diopmame_khalifa7@yahoo.com>
2023-02-07 07:01:51 +0100

    gestion d'erreur

--- conversion_unite_de_mesures.py
+++ conversion_unite_de_mesures.py
@@ -15,7 +15,14 @@ def conversion(unit1:str , unit2:str , facteur:float):
     valeur_str= input(f"conversion {unit1} vers {unit2}. Donner la valeur en {unit1} ou q pour quitter ")
     if valeur_str== "q":
         return True
-    valeur_float= float(valeur_str)
+    try:
+        valeur_float= float(valeur_str)
+    except:
+        # Gestion des erreurs en rebouclant sur la fonction
+        print("ERROR: vous devez entrer une valeur numerique. \n Pour les nombres decimals, mettez un point au lieu de virgule")
+        return conversion(unit1,unit2,facteur)
+    #autre possibilité:
+    # on peut aussi mettre un else a partir d'ici
     valeur_convertie= round(valeur_float * facteur,2)
     print()
     print(f"Resultat de conversion : {valeur_float} {unit1} = {valeur_convertie} {unit2}")
