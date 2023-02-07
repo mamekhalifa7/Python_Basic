774bd70f79bcb539620002962bb7143b22096fe3
Mame_Khalifa <diopmame_khalifa7@yahoo.com>
2023-02-07 06:37:31 +0100

    gestion de la condition de sortie et convertir autant de fois que l'on veut

--- conversion_unite_de_mesures.py
+++ conversion_unite_de_mesures.py
@@ -12,18 +12,25 @@ if choice=="1":
 print(f"Resultat de conversion : {valeur_float} pouces = {valeur_convertie} cm")
 """
 def conversion(unit1:str , unit2:str , facteur:float):
-    valeur_str= input(f"conversion {unit1} vers {unit2}. Donner la valeur en {unit1} ")
+    valeur_str= input(f"conversion {unit1} vers {unit2}. Donner la valeur en {unit1} ou q pour quitter ")
+    if valeur_str== "q":
+        return True
     valeur_float= float(valeur_str)
     valeur_convertie= round(valeur_float * facteur,2)
     print()
     print(f"Resultat de conversion : {valeur_float} {unit1} = {valeur_convertie} {unit2}")
+    return False
 
 print(" Ce programme vous permet d'effectuer des conversions d'unité")
 print("1 - pouces vers cm")
 print("2 - cm vers pouces")
+print("q -  Quitter")
 choice = input("votre choix (1 ou 2): ")
-if choice=="1":
-    print()
-    conversion("pouce","cm",2.54)
-else:
-    conversion("cm","pouce",0.394)
+while True:
+    if choice=="1":
+        if conversion("pouce","cm",2.54):
+            break
+    else:
+        if conversion("cm","pouce",0.394):
+            break
+
