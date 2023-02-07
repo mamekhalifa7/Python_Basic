30060c6c8d40c5ce4fd3ec2accb4ea026e45b664
Mame_Khalifa <diopmame_khalifa7@yahoo.com>
2023-02-07 10:15:58 +0100

    version amelioré avec REVERSE

---
+++ conversion_unite_de_mesures_ameliore.py
@@ -0,0 +1,57 @@
+
+# Raisonnement simple sans fonction
+"""
+print(" Ce programme vous permet d'effectuer des conversions d'unité")
+print("1 - pouces vers cm")
+print("2 - cm vers pouces")
+choice = input("votre choix (1 ou 2): ")
+if choice=="1":
+    valeur_str= input(" conversion pouces vers cm. Donnez la valeur en pouces : ")
+    valeur_float = float(valeur_str)
+    valeur_convertie = round(valeur_float * 2.54, 2)
+print(f"Resultat de conversion : {valeur_float} pouces = {valeur_convertie} cm")
+"""
+def conversion(unit1:str , unit2:str , facteur:float):
+    valeur_str= input(f"conversion {unit1} vers {unit2}. Donner la valeur en {unit1} ou q pour quitter \n")
+    if valeur_str== "q":
+        return True
+    try:
+        valeur_float= float(valeur_str)
+    except:
+        # Gestion des erreurs en rebouclant sur la fonction egalement appelé la recursion
+        print("ERROR: vous devez entrer une valeur numerique. \n Pour les nombres decimals, mettez un point au lieu de virgule")
+        #Recursion
+        return conversion(unit1,unit2,facteur)
+    #autre possibilité:
+    # on peut aussi mettre un else a partir d'ici
+    valeur_convertie= round(valeur_float * facteur,2)
+    print()
+    print(f"Resultat de conversion : {valeur_float} {unit1} = {valeur_convertie} {unit2}")
+    return False
+
+while True:
+    #Menu du choix de conversion
+    print("********************************************************************")
+    print("*    Ce programme vous permet d'effectuer des conversions d'unité  *")
+    print("********************************************************************")
+    print("*  1 - pouces vers cm                                              *")
+    print("*  2 - cm vers pouces                                              *")
+    print("*  q -  Quitter                                                    *")
+    print("********************************************************************")
+    choice = input("votre choix (1 ou 2): \n")
+    if choice == "1" or choice == "2":
+        break
+    print("ERROR: vous devez entrer 1 ou 2")
+
+#Conversion des valeurs
+while True:
+    if choice=="1":
+        if conversion("pouce","cm",2.54):
+            break
+    else:
+        if conversion("cm","pouce",0.394):
+            break
+
+print("******************************************************")
+print("*                   FIN PROGRAMME                    *")
+print("******************************************************")
