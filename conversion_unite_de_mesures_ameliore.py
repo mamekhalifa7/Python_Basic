723a7e2a2fa4a0eb2b828c2ef8d40d343d9f5831
Mame_Khalifa <diopmame_khalifa7@yahoo.com>
2023-02-08 18:25:35 +0100

    Collections: Tuples

--- conversion_unite_de_mesures_ameliore.py
+++ conversion_unite_de_mesures_ameliore.py
@@ -13,7 +13,7 @@ print(f"Resultat de conversion : {valeur_float} pouces = {valeur_convertie} cm")
 """
 def conversion(unit1:str , unit2:str , facteur:float, reverse:bool = False):
     if reverse:
-        unit1, unit2 = unit2, unit2
+        unit1, unit2 = unit2, unit1
         facteur = 1/facteur
 
     """ 
@@ -41,25 +41,55 @@ def conversion(unit1:str , unit2:str , facteur:float, reverse:bool = False):
     print(f"Resultat de conversion : {valeur_float} {unit1} = {valeur_convertie} {unit2}")
     return False
 
-while True:
-    #Menu du choix de conversion
-    print("********************************************************************")
-    print("*    Ce programme vous permet d'effectuer des conversions d'unité  *")
-    print("********************************************************************")
-    print("*  1 - pouces vers cm                                              *")
-    print("*  2 - cm vers pouces                                              *")
-    print("*  q -  Quitter                                                    *")
-    print("********************************************************************")
-    choice = input("votre choix (1 ou 2): \n")
-    if choice == "1" or choice == "2":
-        break
-    print("ERROR: vous devez entrer 1 ou 2")
+
+def demander_utilisateur(min, max):
+    val_str= input(f"entrer une valeur entre {min} et {max} ")
+    try:
+        val_int= int(val_str)
+    except:
+        print("ERROR vous devez entrer une valeur numerique")
+        return demander_utilisateur(min, max)
+    if not min <=  val_int <= max:
+        print(f"vous devez entrer un nombre entre {min} et {max} ")
+    return val_int
+
+
+# gerer le menu avec les collections: tuple
+CONVERSION=(
+    ("pouce","cm",0.39),
+    ("m","cm",100),
+    ("km","miles",0.621371),
+)
+
+#Menu du choix de conversion
+print("********************************************************************")
+print("*    Ce programme vous permet d'effectuer des conversions d'unité  *")
+print("********************************************************************")
+
+i=1
+for c in CONVERSION:
+    unit1=c[0]
+    unit2=c[1]
+    print(f"{i} - conversion {unit1} vers {unit2}")
+    i += 1
+    print(f"{i} - conversion {unit2} vers {unit1}")
+    i += 1
+valeur_maxi = i-1
+choice = demander_utilisateur(1, valeur_maxi)
 
 #Conversion des valeurs
 while True:
+    # Demander les valeurs à convertir à l'utilisateur
+    # Si le choix de l'utilisateur est 1 alors l'index est 0 (premier item de conversion)
+    # Divisé par 2, car on génére des options intermédiares de conversion inverse
+    # choix =2 -> index = 0 mais reverse = True (conversion inverse)
+    index = (choice - 1)//2
+    #True si la valeur est pair
+    reverse = choice % 2 == 0
+    unit1, unit2, facteur = CONVERSION[index]
     # if inline
-        if conversion("pouce","cm",2.54, reverse=False if choice=="1" else True):
-            break
+    if conversion(unit1, unit2, facteur, reverse):
+        break
 
 print("******************************************************")
 print("*                   FIN PROGRAMME                    *")
